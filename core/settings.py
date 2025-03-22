b0ca6ab36372815f77980c26660a6d4f848fa9d8
Creeper Eye <iostream1204@gmail.com>
2025-03-22 08:08:20 +0000

    new

--- core/settings.py
+++ core/settings.py
@@ -18,7 +18,7 @@ POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
 POSTGRES_USER = os.getenv("POSTGRES_USER")
 POSTGRES_NAME = os.getenv("POSTGRES_NAME")
 POSTGRES_HOST = os.getenv("POSTGRES_HOST")
-POSTGRES_PORT = int(os.getenv("POSTGRES_PORT"))
+POSTGRES_PORT = int(os.getenv("POSTGRES_PORT") if not DEBUG else -1)
 
 
 class ColoredFormatter(logging.Formatter):
