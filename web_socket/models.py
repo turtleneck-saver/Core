890b36e0713851b932777667e5df3cc17f52d8ff
Creeper Eye <iostream1204@gmail.com>
2025-04-04 11:15:28 +0000

    log model added

--- web_socket/models.py
+++ web_socket/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
-# Create your models here.
+
+class Description(models.Model):
+    description = models.TextField()
+
+    def __str__(self):
+        return self.description[:50]  # 관리자 페이지에서 보기 좋게
+
+
+class Status(models.Model):
+    status = models.IntegerField()
+
+    def __str__(self):
+        return str(self.status)  # 관리자 페이지에서 보기 좋게
+
+
+class Ip(models.Model):
+    ip = models.CharField(max_length=15)
+
+    def __str__(self):
+        return self.ip  # 관리자 페이지에서 보기 좋게
+
+
+class Log(models.Model):
+    timestamp = models.DateTimeField(auto_now_add=True)
+    status = models.ForeignKey(Status, on_delete=models.CASCADE, default=1)
+    description = models.ForeignKey(
+        Description, on_delete=models.SET_NULL, null=True, blank=True
+    )  # null=True, blank=True 추가
+    ip = models.ForeignKey(Ip, on_delete=models.SET_NULL, null=True, blank=True)  # null=True, blank=True 추가
+
+    def __str__(self):
+        return f"{self.timestamp} - {self.status} - {self.description}"  # 관리자 페이지에서 보기 좋게
