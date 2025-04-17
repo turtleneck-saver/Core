e08f3d9bebb5d5a23ab14070e9628dff67d0a3cc
Creeper Eye <iostream1204@gmail.com>
2025-04-17 05:37:03 +0000

    fix

--- api/celery.py
+++ api/celery.py
@@ -9,7 +9,7 @@ os.environ.setdefault(
     "core.settings",
 )
 
-app = Celery("core")
+app = Celery("api")
 
 # 여기서 문자열을 사용하면 작업자가 구성 개체를 하위 프로세스에 직렬화할 필요가 없습니다.
 # 네임스페이스='CELERY'는 모든 셀러리 관련 구성 키에 'CELERY_' 접두사가 있어야 함을 의미합니다.
@@ -17,7 +17,12 @@ app.config_from_object(
     "django.conf:settings",
     namespace="CELERY",
 )
-
+app.conf.beat_schedule = {
+    "get-resource-metrics-every-1-second": {
+        "task": "api.tasks.get_resource_metrics",  # tasks.py 파일 경로에 맞게 수정
+        "schedule": 1.0,  # 1초마다 실행
+    },
+}
 # 등록된 django apps 내부의 모든 task 모듈을 찾습니다.
 
 app.autodiscover_tasks()
--- api/tasks.py
+++ api/tasks.py
@@ -1,12 +1,19 @@
 from celery import shared_task
 import time
 import logging
+from prometheus_client import Gauge
+import psutil
 
-logger = logging.getLogger("prod")
+CPU_USAGE = Gauge("cpu_usage_percent", "CPU usage percentage")
+MEM_USAGE = Gauge("mem_usage_percent", "Memory usage percentage")
+HDD_USAGE = Gauge("hdd_usage_percent", "HDD usage percentage")
 
 
 @shared_task
-def test_task(a: int, b: int):
-    for i in range(10):
-        time.sleep(2)
-        logger.info(f"test Celery task :{a + b} ")
+def get_resource_metrics():
+    try:
+        CPU_USAGE.set(psutil.cpu_percent())
+        MEM_USAGE.set(psutil.virtual_memory().percent)
+        HDD_USAGE.set(psutil.disk_usage("/").percent)
+    except Exception as e:
+        logging.error(f"Error while getting resource metrics: {e}")
